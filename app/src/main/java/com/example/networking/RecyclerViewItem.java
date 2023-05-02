1021ec1175d5bd82052989fa3f948f990f9360f5
d22samma <d22samma@student.his.se>
2023-05-02 10:44:23 +0200

    Add RecyclerViewItem.java - To represent an item.

---
+++ app/src/main/java/com/example/networking/RecyclerViewItem.java
@@ -0,0 +1,12 @@
+package com.example.networking;
+
+public class RecyclerViewItem {
+    private String title;
+    public RecyclerViewItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+}
